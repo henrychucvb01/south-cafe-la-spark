a84d7aadc3c82ba8c0bdede1344373f64f01042e
henrychucvb01 <henrychucvb01@yahoo.com>
2026-08-22 09:13:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import SparkIntro from "./components/SparkIntro";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+  <SparkIntro>
+     <App />
+   </SparkIntro>
   </React.StrictMode>
 );
 
