0456d3fac5be710f2ab4291a594122ffd7cbb6ef
henrychucvb01 <henrychucvb01@yahoo.com>
2026-08-23 07:29:52 -0700

    Update SchoolDashboard.js

--- src/pages/SchoolDashboard.js
+++ src/pages/SchoolDashboard.js
@@ -146,7 +146,7 @@ function SchoolDashboard({ location, employee, onBack, onEditFinishLine }) {
 
           <div>
             <div className="login-brand-name">SOUTH CAFÉ LA</div>
-            <div className="login-brand-subtitle">SCHOOL HUB</div>
+            <div className="login-brand-subtitle">Finish Line Overview</div>
           </div>
         </div>
 
